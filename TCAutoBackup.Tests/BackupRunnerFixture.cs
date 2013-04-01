d53d665e5361c3bb18e47cb1c844d03a1715effa
John Rummell <john@jrummell.com>
2013-04-01 10:47:14 -0400

    BackupRunnerFixture - added verification for mock file system infos

--- TCAutoBackup.Tests/BackupRunnerFixture.cs
+++ TCAutoBackup.Tests/BackupRunnerFixture.cs
@@ -46,6 +46,8 @@ namespace TCAutoBackup.Tests
 
             mockFileSystem.Verify(fs => fs.DeleteFile(mockTodayFile.Object.FullName), Times.Never());
             mockFileSystem.Verify();
+            mockTodayFile.Verify();
+            mockLastMonthFile.Verify();
         }
     }
 }
